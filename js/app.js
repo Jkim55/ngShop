adc0361b1001c5b6e8cc5b43d2a619c03ed3410d
Ji Kim <jkim55@gmail.com>
2016-11-21 17:39:29 -0700

    building out controllers and routes on app.js

---
+++ js/app.js
@@ -0,0 +1,33 @@
+angular.module('ngShop', ['ng-route'])
+
+angular.module('ngShop').config(function($routeProvider, $locationProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'partials/home.html',
+      controller: 'homeController'
+    })
+    .when('/about', {
+      templateUrl: 'partials/about.html',
+      controller: 'aboutController'
+    })
+    .when('/contact', {
+      templateUrl: 'partials/contact.html',
+      controller: 'contactController'
+    })
+    .when('/shop', {
+      templateUrl: 'partials/shop.html',
+      controller: 'shopController'
+    })
+    .when('/cart', {
+      templateUrl: 'partials/cart.html',
+      controller: 'cartController'
+    })
+    .otherwise('/nope', {
+      templateUrl: 'partials/error.html',
+      controller: 'errorController'
+    })
+  $locationProvider.html5Mode(true)
+})
+
+// TODO: build out partials and controllers
+// TODO: write out stories on waffle.io (forward tim the link, basic start up info, and link to my waffle)
